7363dfda2e9c7fd950b02df213f58c5c84e5027f
Miley Wright <dishaliekwright@yahoo.com>
2020-10-09 15:25:17 -0400

    api data rendering & cards made

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,18 @@
-// Write your Character component here
+import React from 'react';
+import { Card } from 'antd';
+import '../App.css';
+const { Meta } = Card;
+
+export const Character = (props) => {
+    return (
+        <div className='cards'>
+            <Card className='card'
+                hoverable
+                cover={<img className='image'alt="example" src={props.data.image} />}
+            >
+            <Meta title={props.data.name}/>
+            <Meta description={props.data.status}/>
+            </Card>
+        </div>
+    )
+}
